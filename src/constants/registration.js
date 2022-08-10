255103b020532f26882265fd738aa182f05d3edc
ryanreszetnik <rres@rogers.com>
2022-08-10 10:46:13 -0400

    Registration formatting

--- src/constants/registration.js
+++ src/constants/registration.js
@@ -1,5 +1,14 @@
 export const HOW_TO_REGISTER =
   "Registration opens early August and will be done on the same website for all of the faculties. It is now open at the link below! If you have any questions consult our FAQ page and then feel free to contact us on any of our social medias linked with icons at the bottom of the page.";
 
-// export const STEPS_TO_TAKE = 
-//   "Step 1: Fill out the CVENT Registration form + use your McGill e-mail. If you do not use your McGill e-mail, the platform will not let you register.\Step 2: Make note of your CVENT Confirmation number\nStep 3: Click the payment link - you NEED your CVENT Confirmation number to be able to complete payment\nStep 4: Complete the MANDATORY trainings. There are 3 in total: the Sexual Violence training called <q>It Takes All of Us<q>, the <q>Anti-Oppression<q> training and the <q>Frosh 101<q> training all on MyCourses\nStep 5: Come pick up your credentials and gear at your faculties Registration tent (DON'T FORGET YOUR MCGILL ID!)\n*You must complete Steps 1-4 before coming to pick up your credentials and gear*";
+export const STEPS_TO_TAKE = [
+  "Fill out the CVENT Registration form + use your McGill e-mail. If you do not use your McGill e-mail, the platform will not let you register.",
+  "Make note of your CVENT Confirmation number",
+  "Click the payment link - you NEED your CVENT Confirmation number to be able to complete payment",
+  <>
+    Complete the MANDATORY trainings. There are 3 in total: the Sexual Violence
+    training called <q>It Takes All of Us</q>, the <q>Anti - Oppression</q>{" "}
+    training and the <q>Frosh 101</q> training all on MyCourses
+  </>,
+  "Come pick up your credentials and gear at your faculties Registration tent (DON'T FORGET YOUR MCGILL ID!)",
+];
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,6 +1,6 @@
 import { Button, Typography } from "@mui/material";
 import React from "react";
-import { HOW_TO_REGISTER } from "../../constants/registration";
+import { HOW_TO_REGISTER, STEPS_TO_TAKE } from "../../constants/registration";
 
 export default function Register() {
   return (
@@ -19,38 +19,13 @@ export default function Register() {
           <Typography variant="p">{HOW_TO_REGISTER}</Typography>
         </div>
         <div style={{ padding: "20px 25px 50px 25px" }}>
-          <Typography variant="p">
-            <strong>Step 1:</strong> Fill out the CVENT Registration form + use
-            your McGill e-mail. If you do not use your McGill e-mail, the
-            platform will not let you register.{" "}
-          </Typography>
-          <br></br>
-          <br></br>
-          <Typography variant="p">
-            <strong>Step 2:</strong> Make note of your CVENT Confirmation number
-          </Typography>
-          <br></br>
-          <br></br>
-          <Typography variant="p">
-            <strong>Step 3:</strong> Click the payment link - you NEED your
-            CVENT Confirmation number to be able to complete payment
-          </Typography>
-          <br></br>
-          <br></br>
-          <Typography variant="p">
-            <strong>Step 4:</strong> Complete the MANDATORY trainings. There are
-            3 in total: the Sexual Violence training called{" "}
-            <q>It Takes All of Us</q>, the <q>Anti-Oppression</q> training and
-            the <q>Frosh 101</q> training all on MyCourses
-          </Typography>
-          <br></br>
-          <br></br>
-          <Typography variant="p">
-            <strong>Step 5:</strong> Come pick up your credentials and gear at
-            your faculties Registration tent (DON'T FORGET YOUR MCGILL ID!)
-          </Typography>
-          <br></br>
-          <br></br>
+          {STEPS_TO_TAKE.map((step, index) => (
+            <div style={{ paddingBottom: "1em" }}>
+              <Typography variant="p">
+                <strong>Step {index + 1}:</strong> {step}
+              </Typography>
+            </div>
+          ))}
           <Typography variant="p">
             *You must complete Steps 1-4 before coming to pick up your
             credentials and gear*
