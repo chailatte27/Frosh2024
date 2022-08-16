75d590279d24e8c4cdd4943a2dbff4c7fcd38ed7
ryanreszetnik <rres@rogers.com>
2022-08-16 00:21:30 -0400

    fixed linting

--- src/pages/schedule/index.jsx
+++ src/pages/schedule/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useTheme } from "@emotion/react";
 import { Card, CardMedia, Typography } from "@mui/material";
 import ScheduleImage from "../../images/schedule.png";
 
@@ -9,29 +8,28 @@ export default function Schedule() {
       <Typography variant="h1">Schedule</Typography>
 
       <Card
-            style={{
-              width: "fit-content",
-              marginLeft: "auto",
-              marginRight: "auto",
-            
-            }}
-          >
+        style={{
+          width: "fit-content",
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+      >
         <CardMedia
-                component="img"
-                style={{
-                  width: "auto",
-                  height: "auto",
-                  marginLeft: "auto",
-                  marginRight: "auto",
-                  maxWidth: "90%",
-                  objectFit: "contain",
-                  borderRadius: "5px",
-                  marginBottom: "10px",
-                }}
-                image={ScheduleImage}
-                alt="No img"
-              />
-          </Card>
+          component="img"
+          style={{
+            width: "auto",
+            height: "auto",
+            marginLeft: "auto",
+            marginRight: "auto",
+            maxWidth: "90%",
+            objectFit: "contain",
+            borderRadius: "5px",
+            marginBottom: "10px",
+          }}
+          image={ScheduleImage}
+          alt="No img"
+        />
+      </Card>
     </div>
   );
 }
