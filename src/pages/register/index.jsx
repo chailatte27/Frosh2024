6130cf05450ba80fbf69a4236530402d00d44866
Emily Chai <emilychai@Emilys-MacBook-Air.local>
2024-07-23 20:40:57 -0400

    trying to fix conflictsregistration

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,72 +1,46 @@
-import { Button, Typography } from "@mui/material";
-import React from "react";
-import { HOW_TO_REGISTER, STEPS_TO_TAKE } from "../../constants/registration";
+import * as React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-export default function Register() {
+import { FAQQuestions } from "../../constants/faq";
+
+export default function FAQ() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
   return (
-    <div style={{ marginTop: "10px" }}>
-      <Typography variant="h1">Registration</Typography>
-      <div
-        style={{
-          padding: "10px 20px 50px 20px",
-          width: "90%",
-          marginLeft: "auto",
-          marginRight: "auto",
-          maxWidth: "1000px",
-        }}
-      >
-        <div>
-          <Typography variant="p">{HOW_TO_REGISTER}</Typography>
-        </div>
-        <div style={{ padding: "20px 25px 40px 25px" }}>
-        <Typography variant="p">{STEPS_TO_TAKE}</Typography>
-          <div style={{ paddingTop: "2em" }}>
-          <Typography variant="p">
-            *You must complete Steps 1-5 before coming to pick up your
-            credentials and gear*
-          </Typography>
-          </div>
-        </div>
-        <div style={{paddingBottom: "50px"}}>
-          <Typography variant="p">
-            <strong>Reach out to oweek.communications@eusmcgill.ca if you have any questions.</strong>
-          </Typography>
-
-        </div>
-        <div
-          style={{
-            width: "fit-content",
-            marginLeft: "auto",
-            marginRight: "auto",
-            paddingTop: "10px",
-          }}
+    <div
+      style={{
+        maxWidth: "90%",
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginTop: "10px",
+      }}
+    >
+      <Typography variant="h1">FAQ</Typography>
+      {FAQQuestions.map((q, i) => (
+        <Accordion
+          expanded={expanded === q.question}
+          onChange={handleChange(q.question)}
         >
-          <Button
-            color="primary"
-            variant="contained"
-            size="large"
-            href="https://involvement.mcgill.ca/organization/eus"
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1bh-content"
+            id="panel1bh-header"
           >
-            Register Here
-          </Button>
-        </div>
-        {/*<div
-          style={{
-            width: "fit-content",
-            marginLeft: "auto",
-            marginRight: "auto",
-            paddingTop: "10px",
-          }}
-        >
-          <Button
-            variant="contained"
-            size="large"
-            href="https://drive.google.com/file/d/1c3cRhjv1KXxtrfnLfsSKHiImp28Rdzv_/view?usp=sharing" //TODO: Update link
-          >
-            Bursary Form
-          </Button>
-        </div>*/}
-      </div>
+            <Typography sx={{ fontSize: "1.15em" }}>{q.question}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>{q.answer}</Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
     </div>
   );
 }
